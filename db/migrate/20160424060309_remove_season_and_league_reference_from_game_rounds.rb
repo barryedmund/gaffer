fb14a914d897ab00e4b6a5ce9f83fa3aa09878a3
barryedmund <barrywallace.is@gmail.com>
2016-05-05 23:02:50 -0700

    Remove outdated migration

--- db/migrate/20160424060309_remove_season_and_league_reference_from_game_rounds.rb
+++
@@ -1,6 +0,0 @@
-class RemoveSeasonAndLeagueReferenceFromGameRounds < ActiveRecord::Migration
-  def change
-    remove_column :game_rounds, :season_id
-    remove_column :game_rounds, :league_id
-  end
-end
