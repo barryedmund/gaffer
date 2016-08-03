529da6096ba455d22bb921c9b62db2d9db30130a
barryedmund <barrywallace.is@gmail.com>
2016-08-02 23:24:13 -0700

    Removing hardcoding of localhost

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default_url_options[:host] = "localhost:3000"
+  # default_url_options[:host] = "localhost:3000"
   default from: "from@example.com"
 
   def password_reset(user)
