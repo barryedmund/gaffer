91b7ba27db29e5b7ad63888fae0e84079df2a1c7
barryedmund <barrywallace.is@gmail.com>
2016-08-04 00:41:38 -0700

    Remove as not working on iOS Safari

--- app/assets/javascripts/contract.js
+++ app/assets/javascripts/contract.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
-  $('input[name=player_position_dropdown]').change(function() {
-  	if($('#position_dropdown_goalkeeper').is(':checked')) {
-  		$('#contract_player_id').find('option').show();
-			$('#contract_player_id').find('option').filter(':not(:contains("(Goalkeeper)"))').hide();
-			$("#contract_player_id").val($("#contract_player_id option:first").val());
-		}
-		else if($('#position_dropdown_defender').is(':checked')) {
-			$('#contract_player_id').find('option').show();
-			$('#contract_player_id').find('option').filter(':not(:contains("(Defender)"))').hide();
-			$("#contract_player_id").val($("#contract_player_id option:first").val());
-		}
-		else if($('#position_dropdown_midfielder').is(':checked')) {
-			$('#contract_player_id').find('option').show();
-			$('#contract_player_id').find('option').filter(':not(:contains("(Midfielder)"))').hide();
-			$("#contract_player_id").val($("#contract_player_id option:first").val());
-		}
-		else if($('#position_dropdown_forward').is(':checked')) {
-			$('#contract_player_id').find('option').show();
-			$('#contract_player_id').find('option').filter(':not(:contains("(Forward)"))').hide();
-			$("#contract_player_id").val($("#contract_player_id option:first").val());
-		}
-		else {
-			$('#contract_player_id').find('option').show();
-			$("#contract_player_id").val($("#contract_player_id option:first").val());
-		}
-  });
-});
+// $(document).ready(function() {
+//   $('input[name=player_position_dropdown]').change(function() {
+//   	if($('#position_dropdown_goalkeeper').is(':checked')) {
+//   		$('#contract_player_id').find('option').show();
+// 			$('#contract_player_id').find('option').filter(':not(:contains("(Goalkeeper)"))').hide();
+// 			$("#contract_player_id").val($("#contract_player_id option:first").val());
+// 		}
+// 		else if($('#position_dropdown_defender').is(':checked')) {
+// 			$('#contract_player_id').find('option').show();
+// 			$('#contract_player_id').find('option').filter(':not(:contains("(Defender)"))').hide();
+// 			$("#contract_player_id").val($("#contract_player_id option:first").val());
+// 		}
+// 		else if($('#position_dropdown_midfielder').is(':checked')) {
+// 			$('#contract_player_id').find('option').show();
+// 			$('#contract_player_id').find('option').filter(':not(:contains("(Midfielder)"))').hide();
+// 			$("#contract_player_id").val($("#contract_player_id option:first").val());
+// 		}
+// 		else if($('#position_dropdown_forward').is(':checked')) {
+// 			$('#contract_player_id').find('option').show();
+// 			$('#contract_player_id').find('option').filter(':not(:contains("(Forward)"))').hide();
+// 			$("#contract_player_id").val($("#contract_player_id option:first").val());
+// 		}
+// 		else {
+// 			$('#contract_player_id').find('option').show();
+// 			$("#contract_player_id").val($("#contract_player_id option:first").val());
+// 		}
+//   });
+// });
