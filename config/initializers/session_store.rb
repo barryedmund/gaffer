201ace0388f1b3911fb62af56d846552e09b4b79
barryedmund <barrywallace.is@gmail.com>
2016-08-17 20:38:10 -0700

    Change date from_now to milliseconds

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, {
-                                            key: '_gaffer_session'}
+                                            key: '_gaffer_session',
+                                            expire_after: 30000000 }
