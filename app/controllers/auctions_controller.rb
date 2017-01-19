d2ac83201846ab6fc4640c3d854cc785646262d7
barryedmund <barrywallace.is@gmail.com>
2017-01-18 22:06:07 -0800

    Format the new, allow create, create the show, add specs

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,13 +1,30 @@
 class AuctionsController < InheritedResources::Base
   before_action :require_user
+  before_action :set_team_player, only: [:create]
 
   def new
     @auction = Auction.new
   end
 
+  def create
+    @auction = Auction.new(auction_params)
+    
+    respond_to do |format|
+      if @auction.save
+        format.html { redirect_to league_auction_path(@auction.team_player.team.league.id, @auction), notice: 'Player transfer listed.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
   private
   def auction_params
-    params.require(:auction).permit()
+    params.require(:auction).permit(:team_player_id, :minimum_bid, :is_voluntary, :is_active)
+  end
+
+  def set_team_player
+    @team_player = TeamPlayer.find(params[:auction][:team_player_id])
   end
 end
 
