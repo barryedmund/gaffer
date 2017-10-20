55a6572e3e9519a5ede9f03f2248b81ddaa67438
barryedmund <barrywallace.is@gmail.com>
2017-10-20 16:06:22 -0700

    Update incomplete_transfers_with_team_involved uses to provide relation

--- app/models/team_player.rb
+++ app/models/team_player.rb
@@ -77,7 +77,7 @@ class TeamPlayer < ActiveRecord::Base
   end
 
   def active_transfers
-    Transfer.incomplete_transfers_with_team_involved(team).joins(:transfer_items).where('transfer_items.transfer_item_type = ? AND transfer_items.team_player_id = ?', "Player", id)
+    Transfer.incomplete_transfers_with_team_involved(Team.where(id: team.id)).joins(:transfer_items).where('transfer_items.transfer_item_type = ? AND transfer_items.team_player_id = ?', "Player", id)
   end
 
   def number_of_offers
@@ -142,7 +142,7 @@ class TeamPlayer < ActiveRecord::Base
 
   def has_active_transfer_bid_from_team(team_to_check)
     is_this_team_player = false
-    Transfer.incomplete_transfers_with_team_involved(team_to_check).map{ |transfer| is_this_team_player = true if transfer.get_team_player_involved == self }
+    Transfer.incomplete_transfers_with_team_involved(Team.where(id: team_to_check.id)).map{ |transfer| is_this_team_player = true if transfer.get_team_player_involved == self }
     is_this_team_player
   end
 end
